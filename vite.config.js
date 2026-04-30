893b2554b6c7aebfe8ad148c59f50177f304328b
云6之上 <14430372+above-cloud-6@user.noreply.gitee.com>
2026-04-30 16:45:33 +0800

    add base

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 
 export default defineConfig({
+  base: '/AI_Chat_Room/',
   plugins: [vue()],
   server: {
     port: 5173,
